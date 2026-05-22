71b08966cd200567073aec075c67117de43a1b9d
vamshi707 <vamshivmc143@gmail.com>
2026-05-22 23:44:50 +0530

    updated hardware ai

--- backend27/smartmart_api/electrical/views.py
+++ backend27/smartmart_api/electrical/views.py
@@ -35,44 +35,112 @@ def delete_hardware(request, id):
         "message": "Hardware Deleted"
     })
 # AI HARDWARE DETECTION
+
 @api_view(['POST'])
 def detect_hardware(request):
-    # GET IMAGE + CATEGORY FROM FRONTEND
+
+    # GET IMAGE + CATEGORY
+
     image_data = request.data.get("image")
+
     selected_category = request.data.get("category")
+
     # SPLIT BASE64 IMAGE
+
     format, imgstr = image_data.split(';base64,')
+
     # CONVERT IMAGE
+
     image_file = BytesIO(
         base64.b64decode(imgstr)
     )
+
     image = Image.open(image_file)
-    # FIX PNG / JPEG ISSUE
+
+    # FIX PNG/JPG
+
     image = image.convert("RGB")
+
     # SAVE TEMP IMAGE
+
     image_path = "temp_detect_image.jpg"
+
     image.save(image_path)
+
     # RUN AI
+
     ai_result = detect_hardware_ai(
         image_path
     )
+
     detected_category = ai_result["category"]
+
     confidence = ai_result["confidence"]
+
     length_mm = ai_result["length_mm"]
+
     diameter_mm = ai_result["diameter_mm"]
-    # FIND MATCHING PRODUCTS
+
+    # GET PRODUCTS BY CATEGORY
+
     matched_products = HardwareSpec.objects.filter(
+
         category__icontains=selected_category
+
     )
+
+    # FILTER PRODUCTS MANUALLY
+
+    filtered_products = []
+
+    for product in matched_products:
+
+        try:
+
+            product_length = int(
+
+                product.specifications.get(
+
+                    "length_mm",
+
+                    0
+
+                )
+
+            )
+
+            if length_mm <= product_length <= length_mm + 5:
+
+                filtered_products.append(product)
+
+        except:
+
+            pass
+
+    # SERIALIZER
+
     serializer = HardwareSerializer(
-        matched_products,
+
+        filtered_products,
+
         many=True
+
     )
-    # RETURN RESULT
+
+    # RETURN RESPONSE
+
     return Response({
-       "category": selected_category,
+
+        "category": selected_category,
+
         "confidence": confidence,
+
         "length_mm": length_mm,
+
         "diameter_mm": diameter_mm,
+
+        "output_image": ai_result["output_image"],
+
         "products": serializer.data
+
     })
