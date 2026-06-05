5813e3a6b2f93b005efea431c944f3e275592e24
pujitha-25 <pujithachallagundla@gmail.com>
2026-06-05 21:15:56 +0530

    Your commit message

--- backend27/smartmart_api/SearchResult/views.py
+++ backend27/smartmart_api/SearchResult/views.py
@@ -1,7 +1,7 @@
 from django.db.models import Q
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from Beauty.models import BeautyProduct
 from Groceries.models import GroceryProduct
 from fashion.models import FashionProduct
 from electrical.models import HardwareSpec
@@ -31,6 +31,14 @@ def search_products(request):
         Q(description__icontains=query) |
         Q(material__icontains=query)
     )
+    beauty_items = BeautyProduct.objects.filter(
+        Q(name__icontains=query) |
+        Q(category__icontains=query) |
+        Q(brand__icontains=query) |
+        Q(description__icontains=query) |
+        Q(gender__icontains=query) |
+        Q(skin_type__icontains=query)
+)
 
     # GROCERY
     for item in grocery_items:
@@ -61,5 +69,16 @@ def search_products(request):
             "image": item.image,
             "category": item.category,
         })
+    # BEAUTY
+    for item in beauty_items:
+        results.append({
+          "id": item.id,
+          "name": item.name,
+          "price": item.price,
+          "image": item.image,
+          "category": "Beauty",
+          "brand": item.brand,
+          "description": item.description,
+    })
 
     return Response(results)
